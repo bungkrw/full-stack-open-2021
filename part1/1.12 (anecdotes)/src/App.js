1ba7f8227b9699cbd2d3c91bb25c9f60d5520b70
qingqingqingli <kadlqbetty@gmail.com>
2021-04-17 18:19:48 +0200

    update 1.12 anecdotes

--- part1/1.12 (anecdotes)/src/App.js
+++ part1/1.12 (anecdotes)/src/App.js
@@ -3,7 +3,13 @@ import React, { useState } from 'react'
 const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max))
 
 const App = () => {
-  const anecdotes = [
+
+	const points = { 0: 1, 1: 3, 2: 4, 3: 2 }
+	const copy = { ...points }
+	copy[2] += 1
+	console.log(copy)
+
+	const anecdotes = [
     'If it hurts, do it more often',
     'Adding manpower to a late software project makes it later!',
     'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
@@ -12,16 +18,22 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
 
-  const [selected, setSelected] = useState(0)
+  	const [selected, setSelected] = useState(0)
+	const [vote, setVoted] = useState(0)
+
 	const max = 6
-	const handleClick = () => setSelected(selected + 1)
+	const handleAnecdoteClick = () => setSelected(selected + 1)
+	const handleVoteClick = () => setVoted(vote + 1)
+	let randomInt = getRandomInt(max)
 
   return (
     <div>
-      {anecdotes[getRandomInt(max)]}
-			<p>
-				<button onClick={handleClick}>next anecdote</button>
-			</p>
+      {anecdotes[randomInt]}
+			<div>has {vote} votes</div>
+			<div>
+				<button onClick={handleVoteClick}>vote</button><span> </span>
+				<button onClick={handleAnecdoteClick}>next anecdote</button>
+			</div>
     </div>
   )
 }
